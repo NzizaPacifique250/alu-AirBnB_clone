c670eb69184076003bb0d90f120263648e2a107d
nzizapacifique250 <paccynziza@gmail.com>
2024-09-12 19:33:44 +0200

    Worked on the models

--- models/base_model.py
+++ models/base_model.py
@@ -1,37 +1,38 @@
 #!/usr/bin/python3
 import uuid
 import datetime
+import models
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        """Initializes the BaseModel with unique ID and creation time."""
+
+        timeformat = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, timeformat))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            models.storage.new(self)
+
+    def __str__(self):
+        """String representation of the BaseModel instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.datetime.now()
+        models.storage.save()
+
 
     def to_dict(self): 
-        inst_dict = self.__dict__.copy()
-        inst_dict["__class__"] = self.__class__.__name__
-        inst_dict["__created_at__"] = self.created_at.isoformat()
-        inst_dict["__updated_at__"] = self.updated_at.isoformat()
-
-        return inst_dict
-    def __str__(self):
-        class_name = self.__class__.__name__
-
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
-    
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My Firt Model" 
-    my_model.my_number = 90  
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json) 
-    print('JSON format of my_model:')
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+        """Returns a dictionary containing all keys/values of the instance."""
+        result = self.__dict__.copy()
+        result['__class__'] = self.__class__.__name__
+        result['created_at'] = self.created_at.isoformat()
+        result['updated_at'] = self.updated_at.isoformat()
+        return result
