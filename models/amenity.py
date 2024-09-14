76c4d2e4bba045aff769a8ea42d8ab38a24634cc
Stecie06 <n.stecie@alustudent.com>
2024-09-14 20:55:36 +0400

    Updated Amenity

--- models/amenity.py
+++ models/amenity.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Represent an amenity.
+    """Represent an amenity
 
     Attribute:
         name (str): Name of amenity.
